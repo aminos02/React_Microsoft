df3f6f0ddfba6143fa88f8ef76e9f0e9cb792562
aminos02 <azzouzamine02@gmail.com>
2021-08-02 23:16:39 +0200

    AddDetails

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import { render } from "react-dom";
 import SearchParams from "./SearchParams";
-
+import Details from "./Details";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <h1>Adopt Me!</h1>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
